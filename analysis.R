2f67cbf9771eda08c72e6df3012374682382df36
theodore4889 <theodore.lano@gmail.com>
2016-01-10 16:38:57 -0600

    Cannot get markdown file to run yet

--- analysis.R
+++ analysis.R
@@ -1,133 +1,91 @@
----
-title: "Reproducible Research: Peer Assessment 1"
-output: 
-  html_document:
-    keep_md: true
----
 
-
-## Loading and preprocessing the data
-```{r}
+## Load libraries and dataset  
 library(dplyr)  
-data <- read.table(unz("./R/R/activity.zip", "activity.csv"), header=T, quote="\"", sep=",") 
-old.par <- par(mar = c(0, 0, 0, 0))
-```
+data <- read.table(unz("./R/R/activity.zip", "activity.csv"), header=T, quote="\"", sep=",")  
 
 
-## What is mean total number of steps taken per day?
-First, Aggregate data by getting mean total number of steps in a day
-```{r}
+## Mean Total Number of steps` 
+### Aggregate data by getting mean total number of steps in a day  
 data.dateSum <- aggregate(x = data[c("steps")],  
                       FUN = sum,  
                       by = list(date = data$date))  
-```
 
-Create histogram of total steps 
-```{r}
+### Histogram of total steps  
 hist(data.dateSum$steps, 15, xlab = "Total steps in a day", main = "Histogram of total steps in a day")  
-```
 
-Get mean and median values of the total steps in a day 
-```{r}
+### Get mean and median values of the total steps in a day  
 steps_mean <- mean(data.dateSum$steps, na.rm=TRUE)  
 steps_median <- median(data.dateSum$steps, na.rm=TRUE)  
-```
 
 
-## What is the average daily activity pattern?
-Aggregate data by getting mean number of steps per interval (most active interval on average)  
-```{r}
+## Average Daily Activity Pattern  
+### Aggregate data by getting mean number of steps per interval (most active interval on average)  
 data.timeMean <- aggregate(x = data[c("steps")],  
                           FUN = mean,  
                           na.rm = TRUE,  
                           by = list(interval = data$interval))  
-```
 
-Line plot to show mean number of steps at given time intervals
-```{r}
+### Line plot to show mean number of steps at given time intervals
 plot(data.timeMean$interval, data.timeMean$steps, type = "l", xlab = "Time Interval", 
      ylab = "Mean Number of Steps", main = "Mean number of steps taken at measured time intervals")  
-```
 
-Get interval with maximum number of steps
-```{r}
 interval_stepsMax <- data.timeMean[data.timeMean$steps == max(data.timeMean$steps),]$interval  
-```
 
-## Inputing missing values
-Strategy: separate data by day of the week and average the number of steps per given time  
- interval.  Use these averaged time interval values by day to replace appropriate NA values
 
-Get count of NA values 
-```{r}
+## Replace Missing Data (NA values)  
+### Strategy: separate data by day of the week and average the number of steps per given time  
+###  interval.  Use these averaged time interval values by day to replace appropriate NA values
+
+### Get count of NA values  
 n_NA <- sum(is.na(data$steps))  
-```
 
-Add day specification (Monday, Tuesday, Wednesday, etc...) to data points
-```{r}
+### Add Monday, Tuesday, Wednesday, etc specification to data points
 data$day <- weekdays(as.Date(data$date))
-```
 
-For non NA values, group data by day of the week and time interval, then find means of those groups
-```{r}
+### For non NA values, group data by day of the week and time interval, then find means of those groups
 data.dayInt <- data[complete.cases(data),] %>%
   group_by(day, interval) %>%
   summarize(meanSteps = mean(steps))
-```
 
-Make a copy of data and replace NA values with mean values grouped by day of week and time interval.
-For some reason, this command needs to be run twice in order to update all of the NA values.
-The last 288 don't get updated the first time, but do the second time.  Programming, am I right?
-```{r}
+### Copy raw data
 data.replaceNa <- data
+
+### Replace NA values with mean values grouped by day of week and time interval
 data.replaceNa$steps[is.na(data.replaceNa$steps)] <- data.dayInt$meanSteps[
                                                 match(data.replaceNa$day, data.dayInt$day)
                                               & match(data.replaceNa$interval, data.dayInt$interval)
                                              ]
 
+### For some reason, this command needs to be run twice in order to get all of the NA values
 data.replaceNa$steps[is.na(data.replaceNa$steps)] <- data.dayInt$meanSteps[
                                                 match(data.replaceNa$day, data.dayInt$day)
                                               & match(data.replaceNa$interval, data.dayInt$interval)
                                              ]
-```
 
-Aggregate data by getting mean number of steps per interval
-```{r}
+### Aggregate data by getting mean number of steps per interval  
 data.dateSumReplaceNa <- aggregate(x = data.replaceNa[c("steps")],
                           FUN = sum, 
                           by = list(date = data.replaceNa$date))
-```
 
-Histogram of total steps
-```{r}
+# Histogram of total steps
 hist(data.dateSumReplaceNa$steps, 15, xlab = "Total steps per day", main = "Histogram of total steps per day")
-```
 
-Get mean and median values of the total steps in a day
-```{r}
+### Get mean and median values of the total steps in a day  
 stepsRepNa_mean <- mean(data.dateSumReplaceNa$steps, na.rm=TRUE)
 stepsRepNa_median <- median(data.dateSumReplaceNa$steps, na.rm=TRUE)
-```
 
-Both mean and median values increased when replacing NA values with values averaged for 
-intervals as a function of the day of the week.
+#### Both mean and median values increased when replacing NA values with values averaged for 
+####  intervals as a function of the day of the week.
 
-By putting in this missing data, we are strengthing the impact of days which contained the most NA values.
-Data will skew more towards those days
-
-```{r}
+### By putting in this missing data, we are strengthing the impact of days which contained the most NA values.
+### Data will skew more towards those days
 table(data[is.na(data$steps),]$day)
-```
 
-## Are there differences in activity patterns between weekdays and weekends?
 
-New variable that indicates if the day is a weekday or a weekend
-```{r}
+# New variable that indicates if the day is a weekday or a weekend
 data.replaceNa$weekday <- ifelse(substr(weekdays(as.Date(data.replaceNa$date)), 0,1) != 'S', "weekday", "weekend")
-```
 
-Split data from weekdays and weekends
-```{r}
+# Split data from weekdays and weekends
 data.weekdays <- data.replaceNa[data.replaceNa$weekday == "weekday",]
 data.weekends <- data.replaceNa[data.replaceNa$weekday == "weekend",]
 
@@ -140,10 +98,7 @@ data.meanWeekends <- aggregate(x = data.weekends[c("steps")],
                                FUN = mean,
                                na.rm = TRUE,
                                by = list(interval = data.weekends$interval))
-```
 
-Set Plot Attributes and plot
-```{r}
 main_text <- "Mean number of steps taken at measured time intervals"
 xlab_text <- "Time Interval"
 ylab_text <- "Mean Number of Steps"
@@ -158,5 +113,3 @@ plot(data.meanWeekends$interval, data.meanWeekends$steps, type = "l", xlab = xla
      ylab = ylab_text,ylim = y_lim, main = "weekends")
 
 title( main_text, outer = TRUE )
-par(old.par)
-```
